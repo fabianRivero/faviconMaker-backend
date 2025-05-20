45fe2b1d9da0725d4c33df04d695a4e716e51057
fabianRivero <fabianarj467@gmail.com>
2025-05-20 14:34:48 -0400

    ajustes para correr en netlify

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from './app.js';
 
-const host = "localhost";
+const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
