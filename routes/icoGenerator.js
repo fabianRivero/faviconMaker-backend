0d05b617e830d1756079b4d1e687cb2315116e7c
fabianRivero <fabianarj467@gmail.com>
2025-05-20 18:47:59 -0400

    cambios

--- routes/icoGenerator.js
+++ routes/icoGenerator.js
@@ -4,6 +4,13 @@ import multer from "multer";
 import { requireAuth } from "../middlewares/clerkAuth.mjs";
 import { fileURLToPath } from "url";
 import pngToIco from "png-to-ico";
+import { pathToRegexp } from 'path-to-regexp';
+
+express.Router.layer = function(path, options, fn) {
+  const keys = [];
+  const regexp = pathToRegexp(path, keys, options);
+  return { regexp, keys, fn };
+};
 
 const router = express.Router();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
