94acc3ca463ca62c6f9226aa418bf64621dd7ed7
fabianRivero <fabianarj467@gmail.com>
2025-05-20 21:06:25 -0400

    cambio de cors

--- app.js
+++ app.js
@@ -22,13 +22,22 @@ const corsOptions = {
     'http://localhost:3000',
     'https://faviconmaker-backend.onrender.com'
   ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   credentials: true,
+  preflightContinue: false,
   optionsSuccessStatus: 204
 };
 
-app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use((req, res, next) => {
   console.log(`[${req.method}] ${req.originalUrl} Origin: ${req.headers.origin}`);
